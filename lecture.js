e42b0787075469f192c00f1fec15ad2fe6c9fff7
Shubham Paul <iamshubhham@hotmail.com>
2024-11-10 15:07:58 +0530

    Working on building an avengers site

---
+++ lecture.js
@@ -0,0 +1,6 @@
+/* window.addEventListener("scroll", function () {
+  const video = document.querySelector(".video-container video");
+  const speed = 0.5; // Adjust for a stronger or softer effect
+  video.style.transform = "translateY(" + window.scrollY * speed + "px)";
+});
+ */
